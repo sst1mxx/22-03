17f8446937d4bd9700e671815691c350ec69a0a8
sst1mxx <nbsultanbekov@gmail.com>
2023-02-25 11:03:18 +0600

    DZ5

--- DZ5/app.js
+++ DZ5/app.js
@@ -2,17 +2,17 @@ const som = document.querySelector('#som')
 const usd = document.querySelector('#usd')
 const eur = document.querySelector('#eur')
 
-som.addEventListener('input', () => {
-    const request = new XMLHttpRequest()
-    request.open('GET', 'data.json')
-    request.setRequestHeader('Content-type', 'application/json')
-    request.send()
-
-    request.addEventListener('load', () => {
-        const data = JSON.parse(request.response)
-        usd.value = (som.value / data.usd).toFixed(2)
-    })
-})
+// som.addEventListener('input', () => {
+//     const request = new XMLHttpRequest()
+//     request.open('GET', 'data.json')
+//     request.setRequestHeader('Content-type', 'application/json')
+//     request.send()
+//
+//     request.addEventListener('load', () => {
+//         const data = JSON.parse(request.response)
+//         usd.value = (som.value / data.usd).toFixed(2)
+//     })
+// })
 
 const convert = (elem, target, target2, isTrue) => {
     elem.oninput = () => {
